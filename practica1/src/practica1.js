bbfe6f0ae883dd802f3219fbc4a79c83fca97a05
jbasdom <36700003+jbasdom@users.noreply.github.com>
2018-02-21 15:20:19 +0100

    Fixed initGame(), added shuffle()

--- practica1/src/practica1.js
+++ practica1/src/practica1.js
@@ -13,50 +13,26 @@ MemoryGame = function(gs) {
 	this.gs = gs;
 	this.cards =[];
 	this.state = "none";
+	this.message = "Pick first card.";
 	this.cardsFound = 0;
 
 	// Initializes the game creating the cards, shuffling them and starting the game loop
 	this.initGame = function() {
-		int rnd1, rnd2;
-		for (int i = 0; i < 8; i++) {
-			do { rnd1 = Math.floor(Math.random() * 16); } while (this.cards[rnd1] != undefined)
-			do { rnd2 = Math.floor(Math.random() * 16); } while (this.cards[rnd2] != undefined)
+		let types = ["8-ball", "potato", "dinosaur", "kronos", "rocket", "unicorn", "guy", "zeppelin", "back"];
 
-			switch(i) {
-			case 0:  this.cards[rnd1] = "8-ball";
-					 this.cards[rnd2] = "8-ball"; 
-					 break;
-			case 1:  this.cards[rnd1] = "potato";
-				     this.cards[rnd2] = "potato";
-				     break;
-			case 2:  this.cards[rnd1] = "dinosaur";
-				     this.cards[rnd2] = "dinosaur";
-				     break;
-			case 3:  this.cards[rnd1] = "kronos";
-				     this.cards[rnd2] = "kronos";
-				     break;
-			case 4:  this.cards[rnd1] = "rocket";
-				     this.cards[rnd2] = "rocket";
-				     break;
-			case 5:  this.cards[rnd1] = "unicorn";
-				     this.cards[rnd2] = "unicorn";
-				     break;
-			case 6:  this.cards[rnd1] = "guy";
-				     this.cards[rnd2] = "guy";
-				     break;
-			case 7:  this.cards[rnd1] = "zeppelin";
-				     this.cards[rnd2] = "zeppelin";
-				     break;
-			default: this.cards[rnd1] = "back";
-				     this.cards[rnd2] = "back";
+		for (let i = 0; i < 8; i++) {
+			for (let j = 0; j < 2; j++) {
+				this.cards.push(new MemoryGameCard(types[i]));
 			}
 		}
+
+		shuffle(this.cards);
 		this.loop();
 	};
 
 	// Draws the game
 	this.draw = function() {
-		gs._proto_.drawMessage("Pick first card");
+		gs._proto_.drawMessage(this.message);
 		for (int a = 0; a < 8; a++) {
 			gs._proto_.draw(MemoryGame[a]);
 		}
@@ -74,11 +50,11 @@ MemoryGame = function(gs) {
  * Game's cards constructor. Receives as a parameter the name of the sprite representing the card.
  * Two cards will be the same if they have the same sprite.
  * A card can save the position within the table so it can be drawn later
- * @param {string} sprite Name of the sprite representing the card
+ * @param {id} id Name of the sprite representing the card
  */
 
-MemoryGameCard = function(sprite) {
-	this.MemoryGameCard = function(sprite) { }
+MemoryGameCard = function(id) {
+	this.id = id;
 
 	// Flips the card, changing its status
 	this.flip = function() { };
@@ -89,3 +65,11 @@ MemoryGameCard = function(sprite) {
 	// Compares two cards, returning true if both represent the same card
 	this.compareTo = function(otherCard) { };
 };
+
+function shuffle(a) {
+	for (let i = a.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i+1));
+		[a[i], a[j]] = [a[j], a[i]];
+	}
+	return a;
+}
